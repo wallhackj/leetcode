d06593f3bca38893b287989aaf1889fcf49641e1
Omnissiah <victorhaideu@gmail.com>
2024-10-24 20:46:33 +0300

    puzzle 3

--- src/main/java/org/example/puzzle/FileReader.java
+++ src/main/java/org/example/puzzle/FileReader.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-    private static final String PATH = "src/main/java/org/example/puzzle/input.txt";
+    private static final String PATH = "src/main/java/org/example/puzzle/inputs/input.txt";
 
     public static List<String> inputs() {
         try (RandomAccessFile reader = new RandomAccessFile(PATH, "r")) {
---
+++ src/main/java/org/example/puzzle/Main.java
@@ -0,0 +1,53 @@
+package org.example.puzzle;
+
+import static org.example.puzzle.FileReader.inputs;
+
+public class Main {
+    static final int MAX_RED = 12;
+    static final int MAX_GREEN = 13;
+    static final int MAX_BLUE = 14;
+
+    public static void main(String[] args) {
+        var games = inputs();
+        int sumOfPossibleGameIDs = 0;
+
+        for (String game : games) {
+            var split = game.split(":");
+            var subsets = split[1].split(";");
+            var isPossible = true;
+
+            var gameId = Integer.parseInt(split[0].replace("Game ", "").trim());
+
+            for (int i = subsets.length - 1; i >= 0; i--) {
+                int red = 0, green = 0, blue = 0;
+                var cubes = subsets[i].split(",");
+
+                for (var cube : cubes) {
+                    cube = cube.trim();
+                    if (cube.endsWith("red")) {
+                        red = Integer.parseInt(cube.split(" ")[0]);
+                    } else if (cube.endsWith("green")) {
+                        green = Integer.parseInt(cube.split(" ")[0]);
+                    } else if (cube.endsWith("blue")) {
+                        blue = Integer.parseInt(cube.split(" ")[0]);
+                    }
+
+                    if (red > MAX_RED || blue > MAX_BLUE || green > MAX_GREEN) {
+                        isPossible = false;
+                        break;
+                    }
+                }
+
+                if (!isPossible) {
+                    break;
+                }
+            }
+
+            if (isPossible) {
+                sumOfPossibleGameIDs += gameId;
+            }
+        }
+
+        System.out.println("Sum of possible game IDs: " + sumOfPossibleGameIDs);
+    }
+}
